d1e9487f4edd5c3e1b95e1517a70a50e434dd5f9
Surjitpaul29 <paul.surjit55@gmail.com>
2016-10-24 15:17:54 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,6 +1,14 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
+var Pool= require('pg').Pool;
+var config={
+    user:'surjitpaul29',
+    database:'surjitpaul29',
+    host: 'db.imad.hasura.app.io',
+    port: '5432',
+    password: process.env.DB_Password
+};
 
 var app = express();
 app.use(morgan('combined'));
@@ -11,6 +19,18 @@ app.get('/', function (req, res) {
 
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
+});
+var pool=new Pool(config);
+
+app.get('/test-db',function(req,res){
+//make a database query
+pool.query('select * from Student',function(err,result){
+    
+   if(err){
+        res.status(500).send(err.toString());
+    } 
+});
+    
 });
 
 app.get('/ui/madi.png', function (req, res) {
